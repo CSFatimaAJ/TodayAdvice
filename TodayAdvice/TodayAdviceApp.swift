35ad84f7bdfe940221e3ce71d60cbfa99cdc76f4
CSFatimaAJ <csfatimaaljaber@icloud.com>
2022-11-05 21:18:19 +0300

    Initial Commit

---
+++ TodayAdvice/TodayAdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayAdviceApp.swift
+//  TodayAdvice
+//
+//  Created by Fatima Aljaber on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodayAdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
