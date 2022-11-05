968e72596f2a57cf0ddb5dca383a69ad79c29394
CSFatimaAJ <csfatimaaljaber@icloud.com>
2022-11-05 21:20:58 +0300

    Version1

--- TodayAdvice/ContentView.swift
+++ TodayAdvice/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  TodayAdvice
+//  API
 //
 //  Created by Fatima Aljaber on 05/11/2022.
 //
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var TodayAdvice: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack{
+            Image("logo").resizable().frame(width: 400,height: 200)
+            Text("\(TodayAdvice)").fontWeight(.semibold).foregroundColor(Color.black).multilineTextAlignment(.center).padding([.leading, .bottom, .trailing], 30.0)
+            Spacer()
+            Button {
+                Task {
+                    await loadData()
+                }
+            } label: {
+                Text("Today's Advice").bold()
+                    .frame(maxWidth: 250,maxHeight: 50)
+                    .background(
+                        RoundedRectangle(cornerRadius: 6, style: .continuous)
+                            .fill(.green)
+                            .padding(1.8)
+                    )
+                    .foregroundColor(.white)
+            }.padding(.top)
         }
-        .padding()
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://api.adviceslip.com/advice") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(adviceAPI.self, from: data) {
+                TodayAdvice = decodedResponse.slip.advice
+            }
+        } catch {
+            print("Invalid data")
+        }
+        
     }
 }
 
@@ -24,3 +54,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct adviceAPI: Codable {
+    let slip: Slip
+}
+
+struct Slip: Codable {
+    let id: Int
+    let advice: String
+}
+
+
+
